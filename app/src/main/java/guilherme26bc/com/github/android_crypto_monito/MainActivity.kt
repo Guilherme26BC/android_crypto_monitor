3490315b6cba9959fdcca9f64456841fed1330ce
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-31 22:32:15 -0300

    Fist commit

---
+++ app/src/main/java/guilherme26bc/com/github/android_crypto_monito/MainActivity.kt
@@ -0,0 +1,21 @@
+package guilherme26bc.com.github.android_crypto_monito
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import guilherme26bc.com.github.android_crypto_monito.ui.theme.Android_crypto_monitoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
